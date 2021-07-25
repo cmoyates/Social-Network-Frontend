71ae10785293500fdb0f8363ffe4e858284084b1
Cristopher Yates <cmoyates@gmail.com>
2021-07-25 04:30:31 -0230

    Let the frontend communicate with the current backend

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import './App.css';
 import PostCard from "./components/PostCard.js";
+import SubmitPostDialog from './components/SubmitPostDialog';
 import Container from "@material-ui/core/Container";
+import Button from '@material-ui/core/Button';
 import {useState, useEffect} from 'react';
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  //const [post, setPost] = useState([]);
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleCancel = () => {
+    setOpen(false);
+  };
+  const handleSubmit = async (post) => {
+    setOpen(false);
+    console.log(post)
+    const newPost = await fetch("http://localhost:5000/posts", {
+      method: "POST",
+      headers : { 
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+      },
+      body: JSON.stringify(post)
+    });
+    console.log(newPost);
+    await fetchPosts();
+  };
+
+  const fetchPosts = async () => {
+    const res = await fetch('http://localhost:5000/posts');
+    const data = await res.json();
+    document.title = "Social Network"
+
+    console.log(data);
+    setPosts(data);
+  }
   useEffect(() => {
-    const fetchPosts = async () => {
-      const res = await fetch('http://localhost:5000/posts');
-      const data = await res.json();
-      document.title = "Social Network"
-  
-      console.log(data);
-      setPosts(data);
-    }
-  
     fetchPosts();
   }, [])
 
@@ -24,9 +49,11 @@ function App() {
   return (
     <div className="App">
       <h1>Social Network</h1>
+      <Button size={"small"} onClick={handleClickOpen}>Post</Button>
       <Container maxWidth="sm">
-        {posts.map((item) => (<PostCard key={item.id} user={item.user} content={item.content} likeCount={item.likes}/>))}
+        {posts.map((item) => (<PostCard key={item.post_id} user={item.user_name} content={item.content} likeCount={item.likes}/>))}
       </Container>
+      <SubmitPostDialog open={open} handleClose={handleCancel} handleSubmit={handleSubmit}/>
     </div>
   );
 }
---
+++ src/components/SubmitPostDialog.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import {useState} from 'react';
+
+function SubmitPostDialog(props) {
+
+    const [name, setName] = useState("");
+    const [content, setContent] = useState("");
+
+
+
+    return (
+        <div>
+            <Dialog open={props.open} onClose={props.handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Post</DialogTitle>
+                <DialogContent>
+                <TextField
+                    value={name} onChange={(e) => {setName(e.target.value)}}
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Name"
+                />
+                <TextField
+                    value={content} onChange={(e) => {setContent(e.target.value)}}
+                    autoFocus
+                    margin="dense"
+                    id="content"
+                    label="Content"
+                    fullWidth
+                    multiline
+                />
+                </DialogContent>
+                <DialogActions>
+                <Button onClick={async () => {
+                    await props.handleSubmit({
+                        user_name: name,
+                        content: content,
+                        likes: 0
+                    });
+                }} color="primary">
+                    Submit
+                </Button>
+                <Button onClick={props.handleClose} color="primary">
+                    Cancel
+                </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
+
+export default SubmitPostDialog;
