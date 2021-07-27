32251e6b082851bae443cd52c7765e6702f5084f
Cristopher Yates <cmoyates@gmail.com>
2021-07-27 19:07:47 -0230

    Added support for multiple pages

--- src/App.js
+++ src/App.js
@@ -1,52 +1,22 @@
 import './App.css';
-import PostCard from "./components/PostCard.js";
-import SubmitPostDialog from './components/SubmitPostDialog';
-import Container from "@material-ui/core/Container";
-import Button from '@material-ui/core/Button';
-import {useState, useEffect} from 'react';
+import {BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-router-dom";
+import Home from './pages/Home';
+import Posts from './pages/Posts';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [open, setOpen] = useState(false);
-
-  const handleSubmit = async (post) => {
-    setOpen(false);
-    console.log(post)
-    const newPost = await fetch("https://fast-coast-04774.herokuapp.com/posts", {
-      method: "POST",
-      headers : { 
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-      },
-      body: JSON.stringify(post)
-    });
-    console.log(newPost);
-    await fetchPosts();
-  };
-
-  const fetchPosts = async () => {
-    const res = await fetch('https://fast-coast-04774.herokuapp.com/posts');
-    const data = await res.json();
-    document.title = "Social Network"
-
-    console.log(data);
-    setPosts(data);
-  }
-  useEffect(() => {
-    fetchPosts();
-  }, [])
-
-  //style={{backgroundColor: "#293647"}}
 
   return (
-    <div className="App">
-      <h1>Social Network</h1>
-      <Button variant="contained" color="primary" size={"medium"} onClick={() => {setOpen(true);}}><b>Post</b></Button>
-      <Container maxWidth="sm">
-        {posts.map((item) => (<PostCard key={item.post_id} user={item.user_name} content={item.content} likeCount={item.likes}/>))}
-      </Container>
-      <SubmitPostDialog open={open} handleClose={() => {setOpen(false);}} handleSubmit={handleSubmit}/>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/posts" component={Posts}/>
+          <Route exact path="/404" component={PageNotFound}/>
+          <Redirect to="/404"/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Posts.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import PostCard from "../components/PostCard.js";
+import SubmitPostDialog from '../components/SubmitPostDialog';
+import Container from "@material-ui/core/Container";
+import Button from '@material-ui/core/Button';
+import {useState, useEffect} from 'react';
+
+const Posts = () => {
+
+
+    const [posts, setPosts] = useState([]);
+    const [open, setOpen] = useState(false);
+
+    const handleSubmit = async (post) => {
+        setOpen(false);
+        console.log(post)
+        const newPost = await fetch("https://fast-coast-04774.herokuapp.com/posts", {
+        method: "POST",
+        headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+        },
+        body: JSON.stringify(post)
+        });
+        console.log(newPost);
+        await fetchPosts();
+    };
+
+    const fetchPosts = async () => {
+        const res = await fetch('https://fast-coast-04774.herokuapp.com/posts');
+        const data = await res.json();
+        document.title = "Social Network"
+
+        console.log(data);
+        setPosts(data);
+    }
+    useEffect(() => {
+        fetchPosts();
+    }, [])
+
+
+    return (
+        <div>
+            <h1>Social Network</h1>
+            <Button variant="contained" color="primary" size={"medium"} onClick={() => {setOpen(true);}}><b>Post</b></Button>
+            <Container maxWidth="sm">
+                {posts.map((item) => (<PostCard key={item.post_id} user={item.user_name} content={item.content} likeCount={item.likes}/>))}
+            </Container>
+            <SubmitPostDialog open={open} handleClose={() => {setOpen(false);}} handleSubmit={handleSubmit}/>
+        </div>
+    )
+}
+
+export default Posts
